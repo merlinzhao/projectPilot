e8d83584fcc6d8247494ee4890f043344c4201fd
Merlin <merlinzhao@gmail.com>
2019-06-25 20:54:11 -0400

    first commit

---
+++ pilotProject/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  pilotProject
+//
+//  Created by Merlin Zhao on 6/25/19.
+//  Copyright © 2019 Merlin Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ pilotProjectTests/pilotProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  pilotProjectTests.swift
+//  pilotProjectTests
+//
+//  Created by Merlin Zhao on 6/25/19.
+//  Copyright © 2019 Merlin Zhao. All rights reserved.
+//
+
+import XCTest
+@testable import pilotProject
+
+class pilotProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
